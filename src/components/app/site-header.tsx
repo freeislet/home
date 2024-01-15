4b2b86005c05da00398e88eec8239de2f3c8304d
freeislet <freeislet@gmail.com>
2024-01-15 11:09:08 +0900

    site-header 코드 정리

--- src/components/app/site-header.tsx
+++ src/components/app/site-header.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 import { siteConfig } from '@/config/site'
@@ -10,16 +9,6 @@ import { Logo } from '@/components/icons'
 import { ThemeMode } from '@/components/theme-mode'
 
 export function SiteHeader() {
-  const pathname = usePathname()
-  const isActive = (url: string) => pathname === url
-  const navItems = [
-    ['Home', '/'],
-    ['Portfolio 1', '/portfolio1'],
-    ['Portfolio 2', '/portfolio2'],
-    ['Portfolio 3', '/portfolio3'],
-    ['About', '/about'],
-  ]
-
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background-blur">
       <div className="my-container my-flex-row h-14">
@@ -37,18 +26,6 @@ export function SiteHeader() {
         </div>
 
         <div className="ml-auto">
-          <nav className="inline text-sm font-medium hidden">
-            {navItems.map(([title, url], index) => (
-              <Link
-                className={`py-1 px-2 rounded-sm hover:bg-theme-active/30
-                  ${isActive(url) ? 'text-theme-active font-bold' : ''}`}
-                href={url}
-                key={index}
-              >
-                {title}
-              </Link>
-            ))}
-          </nav>
           <div className="inline ml-4">
             <ThemeMode />
           </div>
