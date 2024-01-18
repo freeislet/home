5d144737ae63099d47e98b53215f0e922ba7acbb
freeislet <freeislet@gmail.com>
2024-01-18 16:02:06 +0900

    테마 토글 layout shift 방지 코드 제거

--- src/components/theme-mode.tsx
+++ src/components/theme-mode.tsx
@@ -4,8 +4,6 @@ import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import { BsFillSunFill, BsFillMoonFill } from 'react-icons/bs'
 
-import { cn } from '@/lib/utils'
-
 export function ThemeMode() {
   const [mounted, setMounted] = useState(false)
   const { resolvedTheme, setTheme } = useTheme()
@@ -14,14 +12,12 @@ export function ThemeMode() {
     setMounted(true)
   }, [])
 
-  const invisible = !mounted // layout shift 방지를 위해 mount 전에도 invisible 렌더
+  if (!mounted) return
 
   return (
     <button
-      className={cn('border rounded-md p-1', { invisible })}
-      onClick={() =>
-        resolvedTheme && setTheme(resolvedTheme === 'light' ? 'dark' : 'light')
-      }
+      className="border rounded-md p-1"
+      onClick={() => setTheme(resolvedTheme === 'light' ? 'dark' : 'light')}
     >
       {resolvedTheme === 'light' ? <BsFillSunFill /> : <BsFillMoonFill />}
     </button>
