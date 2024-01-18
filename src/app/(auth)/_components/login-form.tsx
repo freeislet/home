912bf27e17c132417962c473e37712fcf29792e9
freeislet <freeislet@gmail.com>
2024-01-18 16:24:27 +0900

    로그인 폼 수정

---
+++ src/app/(auth)/_components/login-form.tsx
@@ -0,0 +1,121 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+// import { useSearchParams } from 'next/navigation'
+// import { zodResolver } from '@hookform/resolvers/zod'
+// import { useForm } from 'react-hook-form'
+// import * as z from 'zod'
+// import { signIn } from 'next-auth/react'
+
+import { cn } from '@/lib/utils'
+// import { userAuthSchema } from '@/lib/validations/auth'
+import { buttonVariants } from '@/components/ui/button'
+// import { Input } from '@/components/ui/input'
+// import { Label } from '@/components/ui/label'
+// import { toast } from '@/components/ui/use-toast'
+import { GitHubIcon, SpinnerIcon } from '@/components/icons'
+
+interface LoginFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+// type FormData = z.infer<typeof userAuthSchema>
+
+export function LoginForm({ className, ...props }: LoginFormProps) {
+  // const {
+  //   register,
+  //   handleSubmit,
+  //   formState: { errors },
+  // } = useForm<FormData>({
+  //   resolver: zodResolver(userAuthSchema),
+  // })
+  const [isLoading, setIsLoading] = useState(false)
+  const [isGitHubLoading, setIsGitHubLoading] = useState(false)
+  // const searchParams = useSearchParams()
+  const router = useRouter()
+
+  // async function onSubmit(data: FormData) {
+  //   setIsLoading(true)
+
+  //   const signInResult = await signIn('email', {
+  //     email: data.email.toLowerCase(),
+  //     redirect: false,
+  //     callbackUrl: searchParams?.get('from') || '/dashboard',
+  //   })
+
+  //   setIsLoading(false)
+
+  //   if (!signInResult?.ok) {
+  //     return toast({
+  //       title: 'Something went wrong.',
+  //       description: 'Your sign in request failed. Please try again.',
+  //       variant: 'destructive',
+  //     })
+  //   }
+
+  //   return toast({
+  //     title: 'Check your email',
+  //     description: 'We sent you a login link. Be sure to check your spam too.',
+  //   })
+  // }
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="grid gap-2">
+          <div className="grid gap-1">
+            <Label className="sr-only" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              id="email"
+              placeholder="name@example.com"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              disabled={isLoading || isGitHubLoading}
+              {...register('email')}
+            />
+            {errors?.email && (
+              <p className="px-1 text-xs text-red-600">
+                {errors.email.message}
+              </p>
+            )}
+          </div>
+          <button className={cn(buttonVariants())} disabled={isLoading}>
+            {isLoading && (
+              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Sign In with Email
+          </button>
+        </div>
+      </form> */}
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            {/*Or */}continue with
+          </span>
+        </div>
+      </div>
+      <button
+        type="button"
+        className={cn(buttonVariants({ variant: 'outline' }))}
+        onClick={() => {
+          setIsGitHubLoading(true)
+          router.push('/login/github')
+        }}
+        disabled={isLoading || isGitHubLoading}
+      >
+        {isGitHubLoading ? (
+          <SpinnerIcon className="size-4 mr-2" />
+        ) : (
+          <GitHubIcon className="size-4 mr-2" />
+        )}{' '}
+        Github
+      </button>
+    </div>
+  )
+}
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,42 @@
-import { getPageSession } from '@/auth/lucia'
+import { Metadata } from 'next'
 import { redirect } from 'next/navigation'
+// import Link from 'next/link'
 
-const Page = async () => {
+import { getPageSession } from '@/auth/lucia'
+import { Logo } from '@/components/icons'
+import { LoginForm } from '../_components/login-form'
+
+export const metadata: Metadata = {
+  title: 'Login',
+  description: 'Login to your account',
+}
+
+export default async function LoginPage() {
   const session = await getPageSession()
   if (session) redirect('/')
 
   return (
-    <>
-      <h1>Sign in</h1>
-      <a href="/login/github">Sign in with GitHub</a>
-    </>
+    <div className="my-container my-flex-col items-center justify-center">
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <Logo className="size-8 mx-auto" />
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Welcome back
+          </h1>
+          {/* <p className="text-sm text-muted-foreground">
+            Enter your email to sign in to your account
+          </p> */}
+        </div>
+        <LoginForm />
+        {/* <p className="px-8 text-center text-sm text-muted-foreground">
+          <Link
+            href="/register"
+            className="hover:text-brand underline underline-offset-4"
+          >
+            Don&apos;t have an account? Sign Up
+          </Link>
+        </p> */}
+      </div>
+    </div>
   )
 }
-
-export default Page
