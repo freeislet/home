7017967d46a5b34eae8b8dd92ff7fc4bfb88decd
freeislet <freeislet@gmail.com>
2024-01-24 16:32:55 +0900

    hero section 테스트 코드 정리

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -7,35 +7,40 @@ import { cn } from '@/lib/utils'
 interface HeroSectionProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function HeroSection({ className, ...props }: HeroSectionProps) {
-  return (
-    <div className={cn('', className)} {...props}>
-      <Text className="text-6xl font-extrabold leading-normal">free islet's home</Text>
-      <Text className="text-3xl font-mediuem leading-normal" delay={0.2}>
-        Portfolio, Blog
-      </Text>
-      <Text className="text-lg font-light leading-loose" delay={0.6}>
-        ...under construction
-      </Text>
-    </div>
-  )
-}
-
-interface TextProps {
-  children: React.ReactNode
-  className: string
-  duration?: number
-  delay?: number
-}
-
-function Text({ children, className, duration = 0.5, delay = 0 }: TextProps) {
+  const groupVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.2,
+      },
+    },
+  }
   const variants = {
     hidden: { opacity: 0, y: -20 },
-    visible: { opacity: 1, y: 0, transition: { duration, delay } },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.5,
+        ease: 'easeOut',
+      },
+    },
   }
 
   return (
-    <motion.div className={className} variants={variants} initial="hidden" whileInView="visible">
-      {children}
-    </motion.div>
+    <div className={cn('p-20', className)} {...props}>
+      <motion.div variants={groupVariants} initial="hidden" whileInView="visible">
+        <motion.h1 className="text-6xl font-extrabold leading-normal" variants={variants}>
+          free islet's home
+        </motion.h1>
+        <motion.h3 className="text-3xl font-mediuem leading-normal" variants={variants}>
+          Portfolio, Blog
+        </motion.h3>
+        <motion.h6 className="text-lg font-light leading-loose" variants={variants}>
+          ...under construction
+        </motion.h6>
+      </motion.div>
+    </div>
   )
 }
