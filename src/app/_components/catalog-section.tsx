114ebb8b52f59fc2462a66a45b6957629848e050
freeislet <freeislet@gmail.com>
2024-01-26 15:31:52 +0900

    catalog layout 정리

--- src/app/_components/catalog-section.tsx
+++ src/app/_components/catalog-section.tsx
@@ -11,7 +11,7 @@ export function CatalogSection({ className, ...props }: CatalogSectionProps) {
   const bgs = ['bg-[#F8E3E3]', 'bg-[#E3F1F8]', 'bg-[#E3F8EE]', 'bg-[#F4F8E3]', 'bg-[#E6E3F8]', 'bg-[#F8ECE3]']
 
   return (
-    <div className={cn('p-12 pb-[100px] space-y-12 text-gray-500', className)} {...props}>
+    <div className={cn('p-6 md:p-12 pb-[100px] space-y-6 md:space-y-12 text-gray-500', className)} {...props}>
       {docsConfig.catalog.map((item, index) => (
         <CatalogItem key={index} item={item} className={bgs[index % bgs.length]} />
       ))}
@@ -21,18 +21,13 @@ export function CatalogSection({ className, ...props }: CatalogSectionProps) {
 
 function CatalogItem({ item, className }: { item: CatalogItem; className?: string }) {
   return (
-    <section className={cn('flex flex-col sm:flex-row rounded-3xl overflow-hidden', className)}>
-      <div className="flex-none max-w-[30vw] max-h-[80vh] lg:m-6">
-        <Image
-          src={item.imageSrc}
-          alt=""
-          sizes="40vw"
-          className="w-auto h-auto max-h-full m-auto rounded-2xl object-contain"
-        />
+    <section className={cn('flex rounded-3xl overflow-hidden', className)}>
+      <div className="flex-none max-w-[20vw] sm:max-w-[30vw] max-h-[80vh]">
+        <Image src={item.imageSrc} alt="" sizes="40vw" className="w-auto h-auto max-h-full rounded-br-2xl" />
       </div>
-      <div className="p-6 lg:p-12 pl-2">
-        <h3 className="font-bold text-xl mb-4">{item.title}</h3>
-        <span>{item.description}</span>
+      <div className="p-3 sm:p-6">
+        <h5 className="mb-4">{item.title}</h5>
+        <p>{item.description}</p>
       </div>
     </section>
   )
