a49e3bb32f5fb77f27fdfcaba4658c67429dba05
freeislet <freeislet@gmail.com>
2024-02-05 22:49:44 +0900

    isPreview(vercel) 추가

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,6 +6,10 @@ export function isDev() {
   return process.env.NODE_ENV === 'development'
 }
 
+export function isPreview() {
+  return process.env.VERCEL_ENV === 'preview'
+}
+
 export function getBaseUrl() {
   // if (typeof window !== 'undefined')
   //   // browser should use relative path
