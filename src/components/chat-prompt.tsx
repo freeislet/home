81686a4f53561784841424703a61a00f44084626
freeislet <freeislet@gmail.com>
2024-02-06 10:16:46 +0900

    ChatPanel, ChatPrompt 컴포넌트 추가

---
+++ src/components/chat-prompt.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+import { useRouter } from 'next/navigation'
+import { UseChatHelpers } from 'ai/react'
+import Textarea from 'react-textarea-autosize'
+
+import { useEnterSubmit } from '@/hooks/key'
+import { ArrowElbowIcon } from './icons'
+import { Button } from './ui/button'
+
+export interface PromptProps extends Pick<UseChatHelpers, 'input' | 'setInput'> {
+  onSubmit: (value: string) => void
+  isLoading: boolean
+}
+
+export function ChatPrompt({ onSubmit, input, setInput, isLoading }: PromptProps) {
+  const { formRef, onKeyDown } = useEnterSubmit()
+  const inputRef = React.useRef<HTMLTextAreaElement>(null)
+  const router = useRouter()
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [])
+
+  return (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault()
+        if (!input?.trim()) {
+          return
+        }
+        setInput('')
+        await onSubmit(input)
+      }}
+      ref={formRef}
+    >
+      <div className="relative flex flex-col w-full px-8 overflow-hidden max-h-60 grow bg-background sm:rounded-md sm:border sm:px-12">
+        <Textarea
+          ref={inputRef}
+          tabIndex={0}
+          onKeyDown={onKeyDown}
+          rows={1}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Send a message."
+          spellCheck={false}
+          className="min-h-[60px] w-full resize-none bg-transparent px-4 py-[1.3rem] focus-within:outline-none sm:text-sm"
+        />
+        <div className="absolute right-0 top-4 sm:right-4">
+          <Button type="submit" size="icon" disabled={isLoading || input === ''}>
+            <ArrowElbowIcon />
+            <span className="sr-only">Send message</span>
+          </Button>
+        </div>
+      </div>
+    </form>
+  )
+}
