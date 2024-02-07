056f4303f9864bb5895493cfd99daa9a08a89d61
freeislet <freeislet@gmail.com>
2024-02-08 02:06:49 +0900

    contentlayer mdx 포스트 예제 추가

---
+++ src/app/(contents)/web/contentlayer/page.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import { compareDesc, format, parseISO } from 'date-fns'
+import { allPosts, Post } from 'contentlayer/generated'
+
+function PostCard(post: Post) {
+  const MdxContent = useMDXComponent(post.body.code)
+
+  return (
+    <div className="mb-8">
+      <h2 className="mb-1 text-xl">
+        <Link href={post.url} className="text-blue-700 hover:text-blue-900 dark:text-blue-400">
+          {post.title}
+        </Link>
+      </h2>
+      <time dateTime={post.date} className="mb-2 block text-xs text-gray-600">
+        {format(parseISO(post.date), 'LLLL d, yyyy')}
+      </time>
+      <div className="text-sm [&>*]:mb-3 [&>*:last-child]:mb-0">
+        <MdxContent />
+      </div>
+    </div>
+  )
+}
+
+export default function ContentlayerPage() {
+  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+
+  return (
+    <div className="mx-auto max-w-xl py-8">
+      <h1 className="mb-8 text-center text-2xl font-black">Next.js + Contentlayer Example</h1>
+      {posts.map((post, idx) => (
+        <PostCard key={idx} {...post} />
+      ))}
+    </div>
+  )
+}
