f1a05d5823e4ddd833b3ac94c2739f1ad3d7c7b0
freeislet <freeislet@gmail.com>
2024-02-13 15:06:29 +0900

    Image 컴포넌트 추가

---
+++ src/components/image.tsx
@@ -0,0 +1,18 @@
+import NextImage, { ImageProps as NextImageProps } from 'next/image'
+
+import { cn } from '@/lib/utils'
+
+interface ImageProps extends PartialExcept<NextImageProps, 'src'> {}
+
+export function Image({ alt, className, width, height, sizes, ...props }: ImageProps) {
+  return (
+    <NextImage
+      alt={alt ?? ''}
+      className={cn('h-96 w-auto', className)}
+      width={width ?? 0}
+      height={height ?? 0}
+      sizes={sizes ?? '100vw'}
+      {...props}
+    />
+  )
+}
