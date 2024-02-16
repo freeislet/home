335401654b745aa2983c53baac9ec8193df59ada
freeislet <freeislet@gmail.com>
2024-02-16 10:59:41 +0900

    blockly 디폴트 옵션 적용

--- src/app/(contents)/web/visual-scripting/blockly/page.tsx
+++ src/app/(contents)/web/visual-scripting/blockly/page.tsx
@@ -2,14 +2,26 @@
 
 import { useRef } from 'react'
 
-import BlocklyWorkspace, { type BlocklyOptions } from '@/components/blockly-workspace'
+import BlocklyWorkspace, { Blockly, type BlocklyOptions } from '@/components/blockly-workspace'
 import toolbox from '@/components/blockly/toolbox-example'
 import { Button } from '@/components/ui/button'
-import { handleClientScriptLoad } from 'next/script'
 
 export default function BlocklyPage() {
   const blocklyRef = useRef<any>()
-  const options: BlocklyOptions = { toolbox }
+  const options: BlocklyOptions = {
+    toolbox,
+    toolboxPosition: 'end', // <start> | end
+    horizontalLayout: false, // <false>
+    grid: { spacing: 25, length: 3, colour: '#ccc', snap: true },
+    zoom: { controls: true, wheel: true, startScale: 1.0, maxScale: 1, minScale: 0.3 },
+    trashcan: true, // <true>
+    maxTrashcanContents: 32, // <32>
+    renderer: 'geras', // <geras> | thrasos | zelos(scratch-like renderer)
+    theme: Blockly.Themes.Zelos,
+    // media: '../../media/',
+    plugins: {}, // https://developers.google.com/blockly/guides/configure/advanced/using_blockly_apis
+    readOnly: false, // <false>
+  }
 
   const handleRun = () => {
     blocklyRef.current?.run()
