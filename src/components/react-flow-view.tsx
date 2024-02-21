2ae6fca03598681ef5072a8d318d2d37d7460b1b
freeislet <freeislet@gmail.com>
2024-02-21 14:17:13 +0900

    reactflow 노드 추가 테스트

--- src/components/react-flow-view.tsx
+++ src/components/react-flow-view.tsx
@@ -1,7 +1,13 @@
 'use client'
 
-import { useCallback } from 'react'
+import { useRef, useCallback } from 'react'
 import ReactFlow, {
+  ReactFlowProvider,
+  useReactFlow,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Connection,
   Background,
   BackgroundVariant,
   MiniMap,
@@ -9,11 +15,6 @@ import ReactFlow, {
   Panel,
   NodeToolbar,
   NodeResizer,
-  Connection,
-  SelectionMode,
-  useNodesState,
-  useEdgesState,
-  addEdge,
 } from 'reactflow'
 import 'reactflow/dist/style.css'
 
@@ -35,13 +36,6 @@ export interface ReactFlowViewProps {
 function ReactFlowView({ className }: ReactFlowViewProps) {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
-
-  // const [nodes, setNodes] = useState(initialNodes)
-  // const [edges, setEdges] = useState(initialEdges)
-  //
-  // const onNodesChange = useCallback((changes) => setNodes((nds) => applyNodeChanges(changes, nds)), [setNodes])
-  // const onEdgesChange = useCallback((changes) => setEdges((eds) => applyEdgeChanges(changes, eds)), [setEdges])
-
   const onConnect = useCallback((connection: Connection) => setEdges((eds) => addEdge(connection, eds)), [setEdges])
 
   const options = {
@@ -57,19 +51,47 @@ function ReactFlowView({ className }: ReactFlowViewProps) {
   }
 
   return (
-    <ReactFlow className={className} {...options}>
-      <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-      <MiniMap nodeColor={getNodeColor} nodeStrokeWidth={3} zoomable pannable />
-      <Controls />
-      <Panel position="top-left" className="p-2 space-x-2 rounded-md shadow bg-secondary">
-        <span>Panel 테스트</span>
-        <Button size="sm" onClick={() => window.alert('클릭!')}>
-          버튼
-        </Button>
-      </Panel>
-      <NodeToolbar />
-      <NodeResizer />
-    </ReactFlow>
+    <ReactFlowProvider>
+      <ReactFlow className={className} {...options}>
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+        <MiniMap nodeColor={getNodeColor} nodeStrokeWidth={3} zoomable pannable />
+        <Controls />
+        <ReactFlowPanel />
+        <NodeToolbar />
+        <NodeResizer />
+      </ReactFlow>
+    </ReactFlowProvider>
+  )
+}
+
+function ReactFlowPanel() {
+  const reactFlowInstance = useReactFlow()
+  const nodeId = useRef(0)
+  const handleAddNode = useCallback(() => {
+    const id = `new-${++nodeId.current}`
+    const newNode = {
+      id,
+      position: {
+        x: Math.random() * 500,
+        y: Math.random() * 500,
+      },
+      data: {
+        label: `Node ${id}`,
+      },
+    }
+    reactFlowInstance.addNodes(newNode)
+  }, [])
+
+  return (
+    <Panel position="top-left" className="p-2 space-x-2 rounded-md shadow bg-secondary">
+      <span>Panel 테스트</span>
+      <Button size="sm" onClick={() => window.alert('클릭!')}>
+        버튼
+      </Button>
+      <Button size="sm" variant="outline" onClick={handleAddNode}>
+        노드 추가
+      </Button>
+    </Panel>
   )
 }
 
