9b09342506e1aa1dbf112c97f5a3d36bdd3dfaaf
freeislet <freeislet@gmail.com>
2024-02-21 11:29:15 +0900

    reactflow custom node 테스트

--- src/components/react-flow-view.tsx
+++ src/components/react-flow-view.tsx
@@ -17,21 +17,16 @@ import ReactFlow, {
 } from 'reactflow'
 import 'reactflow/dist/style.css'
 
-import { initialNodes, initialEdges } from './reactflow/example'
+import './reactflow/reactflow.css'
+import { nodeTypes, initialNodes, initialEdges, reactflowStyle, minimapNodeColor } from './reactflow/example'
+import { cn } from '@/lib/utils'
 import { Button } from './ui/button'
 
-const nodeColor = (node: any) => {
-  switch (node.type) {
-    case 'input':
-      return '#6ede87'
-    case 'output':
-      return '#6865A5'
-    default:
-      return '#ff0072'
-  }
+export interface ReactFlowViewProps {
+  className?: string
 }
 
-function ReactFlowView() {
+function ReactFlowView({ className }: ReactFlowViewProps) {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
 
@@ -44,24 +39,26 @@ function ReactFlowView() {
   const onConnect = useCallback((connection: Connection) => setEdges((eds) => addEdge(connection, eds)), [setEdges])
 
   const options = {
+    nodeTypes,
     nodes,
     edges,
     onNodesChange,
     onEdgesChange,
     onConnect,
+    style: reactflowStyle,
+    // fit to initialNodes bounds (test)
+    fitView: true,
     // figma-like viewport controls
     selectionMode: SelectionMode.Partial,
     selectionOnDrag: true,
     panOnDrag: [1, 2],
     panOnScroll: true,
-    // fit to initialNodes bounds (test)
-    fitView: true,
   }
 
   return (
-    <ReactFlow {...options}>
+    <ReactFlow className={cn(className, 'reactflow')} {...options}>
       <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
-      <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable />
+      <MiniMap nodeColor={minimapNodeColor} nodeStrokeWidth={3} zoomable pannable />
       <Controls />
       <Panel position="top-left" className="p-2 space-x-2 rounded-md shadow bg-secondary">
         <span>Panel 테스트</span>
--- src/components/reactflow/example.tsx
+++ src/components/reactflow/example.tsx
@@ -1,4 +1,8 @@
-import { Node, Edge } from 'reactflow'
+import { Node, Edge, Position } from 'reactflow'
+
+import TextUpdaterNode from './nodes/text-updater'
+
+export const nodeTypes = { textUpdater: TextUpdaterNode }
 
 export const initialNodes: Node[] = [
   {
@@ -6,23 +10,63 @@ export const initialNodes: Node[] = [
     type: 'input',
     data: { label: 'Input Node' },
     position: { x: 250, y: 25 },
+    style: { backgroundColor: '#6ede87', color: 'white' },
   },
 
   {
     id: '2',
     // you can also pass a React component as a label
     data: { label: <div>Default Node</div> },
-    position: { x: 100, y: 125 },
+    position: { x: 350, y: 125 },
+    style: { backgroundColor: '#ff0072', color: 'white' },
   },
   {
     id: '3',
     type: 'output',
     data: { label: 'Output Node' },
     position: { x: 250, y: 250 },
+    style: { backgroundColor: '#6865A5', color: 'white' },
+  },
+  {
+    id: 'node-1',
+    type: 'textUpdater',
+    position: { x: 0, y: 25 },
+    data: { value: 123 },
+  },
+  {
+    id: 'node-2',
+    type: 'output',
+    targetPosition: Position.Top,
+    position: { x: -100, y: 150 },
+    data: { label: 'node 2' },
+  },
+  {
+    id: 'node-3',
+    type: 'output',
+    targetPosition: Position.Top,
+    position: { x: 60, y: 150 },
+    data: { label: 'node 3' },
   },
 ]
 
 export const initialEdges: Edge[] = [
   { id: 'e1-2', source: '1', target: '2' },
   { id: 'e2-3', source: '2', target: '3', animated: true },
+  { id: 'edge-1', source: 'node-1', target: 'node-2', sourceHandle: 'a' },
+  { id: 'edge-2', source: 'node-1', target: 'node-3', sourceHandle: 'b' },
 ]
+
+export const reactflowStyle = {
+  backgroundColor: '#B8CEFF',
+}
+
+export const minimapNodeColor = (node: Node) => {
+  switch (node.type) {
+    case 'input':
+      return '#6ede87'
+    case 'output':
+      return '#6865A5'
+    default:
+      return '#ff0072'
+  }
+}
