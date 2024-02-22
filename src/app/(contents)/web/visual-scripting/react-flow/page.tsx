5808a3cf6d3c2c37ddaf22fab753a6cea4074782
freeislet <freeislet@gmail.com>
2024-02-22 15:52:46 +0900

    reactflow 설명글 추가

--- src/app/(contents)/web/visual-scripting/react-flow/page.tsx
+++ src/app/(contents)/web/visual-scripting/react-flow/page.tsx
@@ -3,19 +3,23 @@
 import dynamic from 'next/dynamic'
 
 import Loading from '../../_components/loading'
+import ProseLayout from '@/components/prose-layout'
+import ReactFlowMdx from './reactflow.mdx'
 
 const ReactFlowView = dynamic(() => import('@/components/react-flow-view'), { ssr: false, loading: () => <Loading /> })
 
 export default function ReactFlowPage() {
   return (
-    <div className="grid grid-rows-[auto_1fr] min-h-main-height">
-      <div className="m-2">
-        React Flow 테스트{' '}
-        <span>
-          (<a href="https://reactflow.dev/showcase">Showcase</a>)
-        </span>
+    <>
+      <div className="grid grid-rows-[auto_1fr] min-h-main-height">
+        <div className="m-2">
+          <span>▪️ React Flow 테스트</span>
+        </div>
+        <ReactFlowView />
       </div>
-      <ReactFlowView />
-    </div>
+      <ProseLayout>
+        <ReactFlowMdx />
+      </ProseLayout>
+    </>
   )
 }
