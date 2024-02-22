2a0dd8853270ee0949cfba0016f598593ed8920a
freeislet <freeislet@gmail.com>
2024-02-22 10:03:24 +0900

    랜딩 페이지 height 조정

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -32,7 +32,7 @@ export function HeroSection({ className, ...props }: HeroSectionProps) {
   }
 
   return (
-    <div className={cn('relative min-h-96 max-h-main-height h-[50rem]', className)} {...props}>
+    <div className={cn('relative min-h-96 max-h-main-height h-[60rem]', className)} {...props}>
       <Image
         alt="space boy"
         src={heroImage}
