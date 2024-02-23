86b104c58e908aeb3b699e8d38c12c9b9e97ab29
freeislet <freeislet@gmail.com>
2024-02-24 02:20:12 +0900

    react flow 문서 scroll  버튼 스타일 변경

--- src/app/(contents)/_components/scroll-ui.tsx
+++ src/app/(contents)/_components/scroll-ui.tsx
@@ -2,7 +2,7 @@ import { FaAnglesDown, FaArrowUp } from 'react-icons/fa6'
 
 export function DownToDocument() {
   return (
-    <div className="my-flex-row space-x-1 p-0.5 pr-1.5 hover:bg-muted/50 ring-1 rounded-full text-xs text-muted-foreground hover:text-accent-foreground">
+    <div className="my-flex-row p-0.5 pr-1.5 space-x-1 ring-1 rounded-full text-xs text-muted-foreground hover:text-accent-foreground hover:bg-muted/50">
       <FaAnglesDown className="size-6 p-1 border bg-border/30 rounded-full" />
       <span>설명</span>
     </div>
@@ -11,9 +11,9 @@ export function DownToDocument() {
 
 export function ScrollToTop({ text }: { text?: string }) {
   return (
-    <div className="sticky top-header-height my-flex-col items-center pt-2 -mb-10">
+    <div className="sticky top-header-height my-flex-col items-center -mb-7">
       <button
-        className="my-flex-row space-x-1 h-8 p-1 bg-muted ring-1 hover:ring-2 rounded-lg shadow-md text-xs text-muted-foreground hover:text-accent-foreground"
+        className="my-flex-row h-7 px-4 space-x-1 border border-t-0 rounded-b-full shadow-md text-xs text-muted-foreground hover:text-accent-foreground bg-background-blur hover:bg-muted/70"
         onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
       >
         <FaArrowUp />
