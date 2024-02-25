2907e6ed2c06ebd14994b1de04b9cac2a46398b7
freeislet <freeislet@gmail.com>
2024-02-26 06:02:39 +0900

    flume 설명 mdx 추가

--- src/app/(contents)/web/visual-scripting/flume/page.tsx
+++ src/app/(contents)/web/visual-scripting/flume/page.tsx
@@ -1,12 +1,39 @@
 'use client'
 
-import FlumeEditor from '@/components/flume-editor'
+import { useRef } from 'react'
+import dynamic from 'next/dynamic'
+
+import FlumeMdx from './flume.mdx'
+import ProseLayout from '@/components/prose-layout'
+import { DiagramIcon } from '@/components/icons'
+import { ScrollTrigger, ScrollTarget } from '@/components/scroll-trigger'
+import { DownToDocument, ScrollToTop } from '@/app/(contents)/_components/scroll-ui'
+import Loading from '@/app/(contents)/_components/loading'
+
+const FlumeEditor = dynamic(() => import('@/components/flume-editor'), { ssr: false, loading: () => <Loading /> })
 
 export default function FlumePage() {
+  const docRef = useRef(null)
+
   return (
-    <div className="grid grid-rows-[auto_1fr] min-h-main-height">
-      <div className="m-2">Flume 테스트</div>
-      <FlumeEditor />
-    </div>
+    <>
+      <div className="grid grid-rows-[auto_1fr] min-h-main-height">
+        <div className="m-2 my-flex-row">
+          <DiagramIcon className="mr-1" />
+          Flume 테스트
+          <ScrollTrigger targetRef={docRef} className="ml-4">
+            <DownToDocument />
+          </ScrollTrigger>
+        </div>
+        <FlumeEditor />
+      </div>
+      <ScrollTarget ref={docRef} />
+      <div className="my-container relative">
+        <ScrollToTop text="Editor" />
+        <ProseLayout>
+          <FlumeMdx />
+        </ProseLayout>
+      </div>
+    </>
   )
 }
