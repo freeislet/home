2870d9886500efc09c2a36d1b7ec3ba13f9885b7
freeislet <freeislet@gmail.com>
2024-02-26 05:40:12 +0900

    mdx 설명글 scroll 버튼 디자인 조정

--- src/app/(contents)/_components/scroll-ui.tsx
+++ src/app/(contents)/_components/scroll-ui.tsx
@@ -2,8 +2,8 @@ import { FaAnglesDown, FaArrowUp } from 'react-icons/fa6'
 
 export function DownToDocument() {
   return (
-    <div className="my-flex-row p-0.5 pr-1.5 space-x-1 ring-1 rounded-full text-xs text-muted-foreground hover:text-accent-foreground hover:bg-muted/50">
-      <FaAnglesDown className="size-6 p-1 border bg-border/30 rounded-full" />
+    <div className="my-flex-row p-0.5 pr-1.5 ring-1 rounded-full text-xs text-muted-foreground hover:text-accent-foreground hover:bg-muted/50">
+      <FaAnglesDown className="size-4 p-0.5 mr-1 border rounded-full bg-border/30" />
       <span>설명</span>
     </div>
   )
@@ -13,10 +13,10 @@ export function ScrollToTop({ text }: { text?: string }) {
   return (
     <div className="sticky top-header-height my-flex-col items-center -mb-7">
       <button
-        className="my-flex-row h-7 px-4 space-x-1 border border-t-0 rounded-b-full shadow-md text-xs text-muted-foreground hover:text-accent-foreground bg-background-blur hover:bg-muted/70"
+        className="my-flex-row h-7 px-4 border border-t-0 rounded-b-3xl shadow-md text-xs text-muted-foreground hover:text-accent-foreground bg-background-blur hover:bg-muted/70"
         onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
       >
-        <FaArrowUp />
+        <FaArrowUp className="size-5 p-0.5 mr-1 border rounded-md bg-border/50" />
         {text && <span>{text}</span>}
       </button>
     </div>
