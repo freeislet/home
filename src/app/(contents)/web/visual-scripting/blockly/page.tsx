86514e287a0b8ebc6ad7452cf2f868a7b0855c04
freeislet <freeislet@gmail.com>
2024-02-26 14:05:29 +0900

    blockly 문서 추가

--- src/app/(contents)/web/visual-scripting/blockly/page.tsx
+++ src/app/(contents)/web/visual-scripting/blockly/page.tsx
@@ -1,13 +1,25 @@
 'use client'
 
 import { useRef, useState } from 'react'
+import dynamic from 'next/dynamic'
 
-import BlocklyWorkspace, { BlocklyWorkspaceOptions, WorkspaceInstance } from '@/components/blockly-workspace'
+import { BlocklyWorkspaceOptions, WorkspaceInstance } from '@/components/blockly-workspace'
 import options from '@/components/blockly/options-default'
 import toolbox from '@/components/blockly/toolbox-example'
 import { Button } from '@/components/ui/button'
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { CodeBlock } from '@/components/codeblock'
+import BlocklyMdx from './blockly.mdx'
+import ProseLayout from '@/components/prose-layout'
+import { DiagramIcon } from '@/components/icons'
+import { ScrollTrigger, ScrollTarget } from '@/components/scroll-trigger'
+import { DownToDocument, ScrollToTop } from '@/app/(contents)/_components/scroll-ui'
+import Loading from '@/app/(contents)/_components/loading'
+
+const BlocklyWorkspace = dynamic(() => import('@/components/blockly-workspace'), {
+  ssr: false,
+  loading: () => <Loading />,
+})
 
 export default function BlocklyPage() {
   const blocklyRef = useRef<WorkspaceInstance>()
@@ -15,6 +27,7 @@ export default function BlocklyPage() {
   const codeRef = useRef('')
   const [outputOpen, setOutputOpen] = useState(false)
   const outputMessagesRef = useRef<string[]>([])
+  const docRef = useRef(null)
 
   const workspaceOptions: BlocklyWorkspaceOptions = {
     backupOnUnload: true,
@@ -43,44 +56,62 @@ export default function BlocklyPage() {
   }
 
   return (
-    <div className="grid grid-rows-[auto_1fr] min-h-main-height">
-      <div className="my-flex-row m-2 space-x-2">
-        <Button onClick={handleRun} size="sm">
-          Run
-        </Button>
-        <Button onClick={handleClear} size="sm" variant="destructive">
-          Clear
-        </Button>
-        <Button onClick={handleViewCode} size="sm" variant="outline" className="!ml-4">
-          View Code
-        </Button>
-        <Dialog open={codeOpen} onOpenChange={setCodeOpen}>
-          <DialogContent className="max-w-3xl">
-            <DialogHeader>
-              <DialogTitle>Generated Code</DialogTitle>
-            </DialogHeader>
-            {!codeRef.current ? (
-              <span>생성된 코드가 없습니다.</span>
-            ) : (
-              <CodeBlock language="js" value={codeRef.current} />
-            )}
-          </DialogContent>
-        </Dialog>
-        <Dialog open={outputOpen} onOpenChange={setOutputOpen}>
-          <DialogContent className="max-w-3xl">
-            <DialogHeader>
-              <DialogTitle>Output</DialogTitle>
-            </DialogHeader>
-            <div>
-              {outputMessagesRef.current.map((message, index) => (
-                <div key={index}>{message}</div>
-              ))}
-            </div>
-          </DialogContent>
-        </Dialog>
+    <>
+      <div className="grid grid-rows-[auto_1fr] min-h-main-height">
+        <div className="my-flex-row justify-between">
+          <div className="m-2 my-flex-row">
+            <DiagramIcon className="mr-1" />
+            Blockly 테스트
+            <ScrollTrigger targetRef={docRef} className="ml-4">
+              <DownToDocument />
+            </ScrollTrigger>
+          </div>
+          <div className="my-flex-row m-2 space-x-2">
+            <Button onClick={handleRun} size="sm">
+              Run
+            </Button>
+            <Button onClick={handleClear} size="sm" variant="destructive">
+              Clear
+            </Button>
+            <Button onClick={handleViewCode} size="sm" variant="outline" className="!ml-4">
+              View Code
+            </Button>
+            <Dialog open={codeOpen} onOpenChange={setCodeOpen}>
+              <DialogContent className="max-w-3xl">
+                <DialogHeader>
+                  <DialogTitle>Generated Code</DialogTitle>
+                </DialogHeader>
+                {!codeRef.current ? (
+                  <span>생성된 코드가 없습니다.</span>
+                ) : (
+                  <CodeBlock language="js" value={codeRef.current} />
+                )}
+              </DialogContent>
+            </Dialog>
+            <Dialog open={outputOpen} onOpenChange={setOutputOpen}>
+              <DialogContent className="max-w-3xl">
+                <DialogHeader>
+                  <DialogTitle>Output</DialogTitle>
+                </DialogHeader>
+                <div>
+                  {outputMessagesRef.current.map((message, index) => (
+                    <div key={index}>{message}</div>
+                  ))}
+                </div>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+        <BlocklyWorkspace ref={blocklyRef} options={options} toolbox={toolbox} workspaceOptions={workspaceOptions} />
       </div>
-      <BlocklyWorkspace ref={blocklyRef} options={options} toolbox={toolbox} workspaceOptions={workspaceOptions} />
-    </div>
+      <ScrollTarget ref={docRef} />
+      <div className="my-container relative">
+        <ScrollToTop text="Workspace" />
+        <ProseLayout>
+          <BlocklyMdx />
+        </ProseLayout>
+      </div>
+    </>
   )
 }
 
