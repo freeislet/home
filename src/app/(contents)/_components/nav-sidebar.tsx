bc994a36df115c207aec6d7213407c40e991e326
freeislet <freeislet@gmail.com>
2024-02-28 12:35:18 +0900

    sidebar width 고정 (react three fiber html 삽입 시 버그 우회)

--- src/app/(contents)/_components/nav-sidebar.tsx
+++ src/app/(contents)/_components/nav-sidebar.tsx
@@ -3,17 +3,21 @@
 import { usePathname } from 'next/navigation'
 
 import { docsConfig } from '@/config/docs'
+import { cn } from '@/lib/utils'
 import { getBaseNavItem } from '@/lib/nav'
 import { NavTree } from '@/components/nav-tree'
 
-export function NavSidebar() {
+export function NavSidebar({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
   const pathname = usePathname()
 
   const navItem = getBaseNavItem(docsConfig.nav, pathname)
   if (!navItem?.children?.length) return
 
   return (
-    <aside className="flex-none hidden sm:flex flex-col min-w-32 space-y-2 bg-secondary border-r">
+    <aside
+      className={cn('flex-none hidden sm:flex flex-col min-w-32 space-y-2 bg-secondary border-r', className)}
+      {...props}
+    >
       <div className="my-flex-row p-4 pb-2 space-x-1 font-medium text-foreground/80 border-b mb-2">
         {navItem.icon}
         <span>{navItem.title}</span>
