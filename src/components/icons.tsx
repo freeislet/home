b46e883abdc75f38956e669c0254bc0f5c168eba
freeislet <freeislet@gmail.com>
2024-03-02 02:59:29 +0900

    icons svg 경고 수정

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -58,8 +58,8 @@ export const GitHubIcon = (props: SvgIconProps) => (
 export const ThreeJsIcon = (props: SvgIconProps) => (
   <SvgIcon
     fill="none"
-    stroke-linecap="square"
-    stroke-miterlimit="10"
+    strokeLinecap="square"
+    strokeMiterlimit="10"
     version="1.1"
     viewBox="0 0 226.77 226.77"
     xmlns="http://www.w3.org/2000/svg"
@@ -67,11 +67,11 @@ export const ThreeJsIcon = (props: SvgIconProps) => (
   >
     <g
       transform="translate(8.964 4.2527)"
-      fill-rule="evenodd"
+      fillRule="evenodd"
       stroke="currentColor"
-      stroke-linecap="butt"
-      stroke-linejoin="round"
-      stroke-width="4"
+      strokeLinecap="butt"
+      strokeLinejoin="round"
+      strokeWidth="4"
     >
       <path d="m63.02 200.61-43.213-174.94 173.23 49.874z" />
       <path d="m106.39 50.612 21.591 87.496-86.567-24.945z" />
