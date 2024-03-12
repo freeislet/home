b1b0814388e83a8cfa8e0c5fad74b58743b1084d
freeislet <freeislet@gmail.com>
2024-03-12 14:44:28 +0900

    show camera 조정 (ignored build step 테스트)

--- apps/main/src/app/(contents)/game/react-three-fiber/shoe/page.tsx
+++ apps/main/src/app/(contents)/game/react-three-fiber/shoe/page.tsx
@@ -17,7 +17,7 @@ export default function ReactThreeFiberBasicDemoPage() {
         Shoe configurator
       </div>
       <div className="relative">
-        <ThreeCanvas shadows camera={{ position: [0, 0, 4], fov: 45 }}>
+        <ThreeCanvas shadows camera={{ position: [0, 0, 3], fov: 45 }}>
           <ambientLight intensity={0.7} />
           <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
           <Shoe />
