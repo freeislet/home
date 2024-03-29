0ec22d80f1c65786d2d4f386bb04d175a07cb984
freeislet <freeislet@gmail.com>
2024-03-29 15:57:32 +0900

    MyPortfolio로 컴포넌트 이름 변경

--- apps/main/src/app/_components/my-portfolio.tsx
+++ apps/main/src/app/_components/my-portfolio.tsx
@@ -1,11 +1,11 @@
 import PortfolioList from '@/components/portfolio-list'
 import { portfolios, PortfolioId } from '@/config/docs/portfolio'
 
-export interface PortpolioProps {
+export interface MyPortpolioProps {
   id: PortfolioId
 }
 
-export default function Portfolio({ id }: PortpolioProps) {
+export default function MyPortfolio({ id }: MyPortpolioProps) {
   const portfolio = portfolios[id]
   return <PortfolioList portfolio={portfolio} />
 }
