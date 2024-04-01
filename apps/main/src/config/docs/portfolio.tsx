ce15fd68f897b49a175112fd454d8189dd570444
freeislet <freeislet@gmail.com>
2024-04-01 09:45:07 +0900

    이미지 폴더명을 contents에서 image로 변경

--- apps/main/src/config/docs/portfolio.tsx
+++ apps/main/src/config/docs/portfolio.tsx
@@ -9,31 +9,31 @@ export const portfolios: PortfolioMap = {
   'three-js': [
     {
       href: '/game/three-js/basic-demo',
-      thumbnail: '/contents/three-js/basic-demo.png',
+      thumbnail: '/image/three-js/basic-demo.png',
       title: 'Basic demo',
       description: 'Basic demo',
     },
     {
       href: '/game/three-js/html',
-      thumbnail: '/contents/three-js/html.png',
+      thumbnail: '/image/three-js/html.png',
       title: 'HTML',
       description: 'HTML',
     },
     {
       href: '/game/three-js/shoe',
-      thumbnail: '/contents/three-js/shoe.png',
+      thumbnail: '/image/three-js/shoe.png',
       title: 'Shoe configurator',
       description: 'Shoe configurator',
     },
     {
       href: '/game/three-js/portal',
-      thumbnail: '/contents/three-js/portal.png',
+      thumbnail: '/image/three-js/portal.png',
       title: 'Portal shader',
       description: 'Portal shader',
     },
     {
       href: '/game/three-js/particles',
-      thumbnail: '/contents/three-js/particles.png',
+      thumbnail: '/image/three-js/particles.png',
       title: 'Particles & Effects',
       description: 'Particles & Effects',
     },
@@ -41,13 +41,13 @@ export const portfolios: PortfolioMap = {
   unity: [
     {
       href: '/game/unity/kart',
-      thumbnail: '/contents/unity/kart.png',
+      thumbnail: '/image/unity/kart.png',
       title: 'Kart',
       description: 'Kart',
     },
     {
       href: '/game/unity/3d-game-kit',
-      thumbnail: '/contents/unity/3d-game-kit.png',
+      thumbnail: '/image/unity/3d-game-kit.png',
       title: '3D Game Kit',
       description: '3D Game Kit',
     },
@@ -55,26 +55,26 @@ export const portfolios: PortfolioMap = {
   mediapipe: [
     {
       href: '/ai/mediapipe/face-tracking',
-      thumbnail: '/contents/mediapipe/face-tracking.png',
+      thumbnail: '/image/mediapipe/face-tracking.png',
       title: 'Face Tracking',
       description: 'Face Landmarker를 이용한 실시간 face tracking 예제.',
     },
     {
       href: '/ai/mediapipe/face-avatar',
-      thumbnail: '/contents/mediapipe/face-avatar.png',
+      thumbnail: '/image/mediapipe/face-avatar.png',
       title: 'Face Avatar',
       description:
         'Face Landmarker의 facialTransformationMatrixes, faceBlendshapes를 이용한 실시간 face avatar 렌더링 예제.',
     },
     {
       href: '/ai/mediapipe/hand-tracking',
-      thumbnail: '/contents/mediapipe/hand-tracking.png',
+      thumbnail: '/image/mediapipe/hand-tracking.png',
       title: 'Hand Tracking',
       description: 'Hand Landmarker를 이용한 실시간 hand tracking 예제.',
     },
     {
       href: '/ai/mediapipe/pose-tracking',
-      thumbnail: '/contents/mediapipe/pose-tracking.png',
+      thumbnail: '/image/mediapipe/pose-tracking.png',
       title: 'Pose Tracking',
       description: 'Pose Landmarker를 이용한 실시간 pose tracking 예제.',
     },
