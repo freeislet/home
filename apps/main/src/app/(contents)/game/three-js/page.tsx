b402a43e273601f5ba986856f50cf30b2185c9ec
freeislet <freeislet@gmail.com>
2024-04-02 14:19:54 +0900

    ThreeJsPage 컴포넌트명 수정

--- apps/main/src/app/(contents)/game/three-js/page.tsx
+++ apps/main/src/app/(contents)/game/three-js/page.tsx
@@ -1,7 +1,7 @@
 import ThreeJsMdx from './three-js.mdx'
 import ProseLayout from '@/components/prose-layout'
 
-export default function ReactThreeFiberPage() {
+export default function ThreeJsPage() {
   return (
     <ProseLayout container>
       <ThreeJsMdx />
