705791016cf11fe87074ebd185450d9409310599
freeislet <freeislet@gmail.com>
2024-04-15 18:47:46 +0900

    github callback 예전 코드 제거

--- apps/main/src/app/(auth)/login/github/callback/route.ts
+++ apps/main/src/app/(auth)/login/github/callback/route.ts
@@ -2,7 +2,6 @@ import type { NextRequest } from 'next/server'
 import { OAuth2RequestError } from 'arctic'
 
 import { getStoredState, githubAuth } from '@/auth/oauth/github'
-import { filterDiff, isEmpty } from '@/lib/utils'
 
 export const GET = async (request: NextRequest) => {
   const url = new URL(request.url)
@@ -26,59 +25,6 @@ export const GET = async (request: NextRequest) => {
         Location: '/',
       },
     })
-
-    // const { githubUser, getExistingUser, createUser } = await githubAuth.validateCallback(code)
-
-    // const userDbAttributes: UserDbAttributes = {
-    //   username: githubUser.login,
-    //   email: githubUser.email,
-    //   avatar_url: githubUser.avatar_url,
-    // }
-
-    // const getUser = async () => {
-    //   const existingUser = await getExistingUser()
-    //   if (existingUser) {
-    //     const existingUserDbAttributes = getUserDbAttributes(existingUser)
-    //     const updatedDbAttributes = filterDiff(userDbAttributes, existingUserDbAttributes)
-    //     if (isEmpty(updatedDbAttributes)) return existingUser
-
-    //     try {
-    //       const updatedUser = await auth.updateUserAttributes(existingUser.userId, updatedDbAttributes)
-    //       return updatedUser
-    //     } catch (e) {
-    //       if (e instanceof LuciaError && e.message === `AUTH_INVALID_USER_ID`) {
-    //         // invalid user id
-    //       }
-    //       // provided user attributes violates database rules (e.g. unique constraint)
-    //       // or unexpected database errors
-    //       throw e
-    //     }
-    //   }
-
-    //   auth.createUser
-    //   const user = await createUser({
-    //     attributes: userDbAttributes,
-    //   })
-    //   return user
-    // }
-
-    // const user = await getUser()
-    // const session = await auth.createSession({
-    //   userId: user.userId,
-    //   attributes: {},
-    // })
-    // const authRequest = auth.handleRequest(request.method, {
-    //   cookies,
-    //   headers,
-    // })
-    // authRequest.setSession(session)
-
-    // return new Response(null, {
-    //   status: 302,
-    //   headers: {
-    //     Location: '/', // redirect to profile page
-    //   },
-    // })
   } catch (e) {
     if (e instanceof OAuth2RequestError) {
       // bad verification code, invalid credentials, etc
